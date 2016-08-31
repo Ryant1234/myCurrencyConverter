07f7e734032717c591322e3dd9b3891ec115a189
Ryan T <ryant1234@gmail.com>
2016-08-31 20:18:17 +1200

    Add these files

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// Need to change the "selected" of the drop down menu
+
+
+// Get button click going, get json data request going
+// Remember to use console.log
+
+//<label for="CurrencyFrom">
+
+
+//var url = "http://api.fixer.io/latest?base=" + base;
+
+// base = exchangeFrom value
+
+//console.log(base);
+var data = "data";
+
+//console.log(base);
+
+
+$("#btnGetExchangeRate").click(function getExchangeRate() {
+
+  $.ajax({
+    url: "http://api.fixer.io/latest?base=" +
+    // What is another way of getting this variable?? Read more about variable context  
+    $("#CurrencyFrom option:selected").val(),
+    beforeSend: function beforeSend(xhrObj) {
+      // Request headers
+      xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+      xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "d342c8d19d4e4aafbf64ed9f025aecc8");
+    },
+    type: "GET",
+    data: data
+
+  }).done(function (data) {
+    if (data.length != 0) {
+
+      // Get the returned data
+      var _currencyFrom = $("#CurrencyFrom option:selected").val();
+      var _currencyTo = $("#CurrencyTo option:selected").val();
+      var exchangeRate = data.rates[_currencyTo];
+
+      lblFrom.innerText = _currencyFrom;
+      lblTo.innerText = _currencyTo;
+      console.log(exchangeRate);
+      $("#txtTo").val(exchangeRate * $("#txtFrom").val());
+    }
+
+    //})
+    //
+  });
+});
+//# sourceMappingURL=main.js.map
